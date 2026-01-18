cd07ea3034a59c2afd82ec713b0b8ab27baece0c
MingweiHu <718199906@qq.com>
2026-01-18 19:55:50 +0800

    system_data

---
+++ System/shuru/Assets/Scripts/TcpServer.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text;
+
+public class TcpServer : MonoBehaviour {
+
+    public string msg;
+    public int tcp_port;
+
+    //以下默认都是私有的成员
+    Socket serverSocket;                //服务器端socket
+    Socket clientSocket;                //客户端socket
+    IPEndPoint ipEnd; 					//侦听端口
+    string recvStr; 					//接收的字符串
+    string sendStr;						//发送的字符串
+    byte[] recvData;                    //接收的数据，必须为字节
+    byte[] sendData = new byte[1024];   //发送的数据，必须为字节
+    int recvLen;                        //接收的数据长度
+
+	// Use this for initialization
+	void Start () {
+		InitSocket();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnApplicationQuit(){
+
+        SocketQuit();
+    }
+
+    void OnDestroy(){
+
+        SocketQuit();
+    }
+
+	//初始化
+    void InitSocket(){
+
+        //定义侦听端口,侦听任何IP
+        ipEnd = new IPEndPoint(IPAddress.Parse("127.0.0.1"), tcp_port);
+        //定义套接字类型,在主线程中定义
+        serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        //连接
+        serverSocket.Bind(ipEnd);
+        //开始侦听,最大5个连接
+        serverSocket.Listen(5);
+
+        //开启一个线程连接，必须的，否则主线程卡死
+        Thread serverThread = new Thread(StartServer);
+        serverThread.Start(serverSocket);
+    }
+
+    //等待连接
+    void StartServer(object obj){
+        
+        string str;
+        while (true)
+        {
+            //等待接收客户端连接 Accept方法返回一个用于和该客户端通信的Socket
+            Socket recviceSocket = ((Socket)obj).Accept();
+            //获取客户端ip和端口号
+            str = recviceSocket.RemoteEndPoint.ToString();
+            // socketList.Add(str, recviceSocket);
+            Debug.Log("Connect with " + str);
+            //Accept()执行过后 当前线程会阻塞 只有在有客户端连接时才会继续执行
+            //创建新线程,监控接收新客户端的请求数据
+            Thread thread = new Thread(SocketReceive);
+            thread.IsBackground = true;
+            thread.Start(recviceSocket);
+        }
+    }
+
+	void SocketSend(string sendStr){
+
+        //清空发送缓存
+        sendData = new byte[1024];
+        //数据类型转换
+        sendData = Encoding.ASCII.GetBytes(sendStr);
+        //发送
+        clientSocket.Send(sendData, sendData.Length, SocketFlags.None);
+    }
+
+	//服务器接收
+    void SocketReceive(object obj){
+
+        while (true)
+        {
+            recvData = new byte[1024];
+            //recvData = new byte[1];
+            //获取收到的数据的长度
+            recvLen = ((Socket)obj).Receive(recvData);
+            if (recvLen == 0)
+			{
+                Debug.Log("no connection");
+                ((Socket)obj).Close();
+                break;
+            }
+            else
+            {
+                msg = Encoding.UTF8.GetString(recvData, 0, recvLen);
+
+                Debug.Log(msg);
+                // posArray = pos.Split(' ');
+                // num_pos = int.Parse(posArray[0]);
+            }
+        }
+    }
+
+	//连接关闭
+    void SocketQuit(){
+
+        //关闭服务器
+        serverSocket.Close();
+        Debug.Log("socket closed");
+    }
+}
---
+++ System/shuru/Assets/Scripts/c_1.cs
@@ -0,0 +1,187 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+public class c_1 : MonoBehaviour
+{
+    public TcpServer serverData;
+    public int block_num;
+    public int rounds;
+    public bool isrecorded = false;
+
+    private bool flag_start = false;
+    private int Rno = 0;
+    private int rand_num;
+    private int result;
+    private Transform g;
+    public GameObject panduan;
+    private Dictionary<string, int> code_dic = new Dictionary<string, int>();
+
+    private StreamWriter sw;
+
+    private DateTime startTime, currentTime;
+    private TimeSpan deltaTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //panduan.transform.GetComponent<MeshRenderer>().material.color = Color.blue;
+        for (int i = 1; i <= 26; i++)
+        {
+            //code_dic.Add(((char)(i + 96)).ToString(), i);
+            code_dic.Add((i + 96).ToString(), i);
+        }
+        if (isrecorded)
+        {
+            sw = new StreamWriter(Application.dataPath + "/DataRecords/data " + transform.name + " " + string.Format("{0}", DateTime.Now.ToString("yyyy-MM-dd hh-mm-ss")) + ".csv");
+            sw.WriteLine("Rno,Expected_choice,User_choice,Spent_time,Result");//毫秒
+        }
+    }
+
+    private void Update()
+    {
+        currentTime = DateTime.Now;
+        if (Rno > rounds)
+        {
+            ProgramExit();
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            flag_start = !flag_start;
+            if (flag_start)
+            {
+                rand_num = 1;
+                g = transform.Find("Block_" + rand_num);
+                Rno = 1;
+                g.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/matHighlight");
+                startTime = DateTime.Now;
+            }
+            else
+            {
+                ProgramExit();
+            }
+        }
+        if (serverData.msg.Length > 0 && flag_start)
+        {
+            string[] msg2 = serverData.msg.Split(' ');
+            string item = msg2[0];
+            int item_a = int.Parse(item);
+            //Debug.Log(item_a);
+            if (item_a <= 20)
+            {
+                //item.ToLower();
+
+                //处理 记录
+                //result = (code_dic[item] == rand_num);
+
+                //result = (item_a == rand_num);
+                if (item_a == rand_num)
+                {
+                    //Debug.Log("102");
+                    panduan.transform.GetComponent<MeshRenderer>().material.color = Color.green;
+                    result = 1;
+
+
+                }
+                else
+                {
+                    //Debug.Log("201");
+                    panduan.transform.GetComponent<MeshRenderer>().material.color = Color.red;
+                    result = 0;
+                }
+                deltaTime = currentTime - startTime;
+                if (isrecorded)
+                {
+                    //Debug.Log("Round: " + Rno);
+                    //Debug.Log("input key: " + item);
+                    //Debug.Log("delte time: " + deltaTime.ToString());
+
+                    //sw.WriteLine(Rno + "," + rand_num + "," + code_dic[item] + "," + deltaTime.TotalMilliseconds + "," + result);
+                    sw.WriteLine(Rno + "," + rand_num + "," + item_a + "," + deltaTime.TotalMilliseconds + "," + result);
+                }
+                //next
+                if (Rno <= rounds)
+                {
+                    Rno++;
+                    //panduan.transform.GetComponent<MeshRenderer>().material.color = Color.white;
+                    g.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/matNone");
+                    rand_num = 1; 
+                    g = transform.Find("Block_" + rand_num);
+                    g.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/matHighlight");
+                    startTime = DateTime.Now;
+                }
+            }
+            //switch (item.Length)
+            //{
+            //    case 1:
+            //        //if (Regex.Match(item, @"^[A-Za-z]+$").Success)
+
+            //        if (item_a<=20)
+            //        {
+            //            //item.ToLower();
+
+            //            //处理 记录
+            //            //result = (code_dic[item] == rand_num);
+            //            result = (item_a == rand_num);
+            //            deltaTime = currentTime - startTime;
+            //            if (isrecorded)
+            //            {
+            //                //Debug.Log("Round: " + Rno);
+            //                //Debug.Log("input key: " + item);
+            //                //Debug.Log("delte time: " + deltaTime.ToString());
+
+            //                //sw.WriteLine(Rno + "," + rand_num + "," + code_dic[item] + "," + deltaTime.TotalMilliseconds + "," + result);
+            //                sw.WriteLine(Rno + "," + rand_num + "," + item_a + "," + deltaTime.TotalMilliseconds + "," + result);
+            //            }
+            //            //next
+            //            if (Rno <= rounds)
+            //            {
+            //                Rno++;
+            //                g.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/matNone");
+            //                for (int i = rand_num; i == rand_num;) { rand_num = UnityEngine.Random.Range(1, block_num + 1); }
+            //                g = transform.Find("Block_" + rand_num);
+            //                g.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/matHighlight");
+            //                startTime = DateTime.Now;
+            //            }
+            //        }
+            //        else
+            //        {
+            //            Debug.Log("invalid value0: " + item);
+            //        }
+            //        break;
+            //    default:
+            //        Debug.Log("invalid value: " + item);
+            //        break;
+            //}
+        }
+        serverData.msg = "";
+    }
+
+    private void ProgramExit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    private void OnDestroy()
+    {
+        if (isrecorded)
+        {
+            sw.Close();
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        if (isrecorded)
+        {
+            sw.Close();
+        }
+    }
+}
